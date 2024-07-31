62c6601a6f00be357351b2f42a36a934661c70fb
Elcortado <josealejandrobermudez@hotmail.com>
2024-07-31 14:30:50 -0300

    estructura-if

---
+++ estructurado/EstructuraIF.java
@@ -0,0 +1,11 @@
+package estructurado;
+
+public class EstructuraIF {
+    public static void main(String[] args) {
+        int topPuntaje = 99;
+        if (topPuntaje <100 ) {
+            System.out.println("El valor del pntaje es: " + topPuntaje);
+    }
+    System.out.println("fuera de el");
+}
+}
