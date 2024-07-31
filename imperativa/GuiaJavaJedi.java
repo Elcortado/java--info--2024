88d95add41306bddfe5e994c649d4b050bbc2697
Elcortado <josealejandrobermudez@hotmail.com>
2024-07-31 14:26:57 -0300

    guia

---
+++ imperativa/GuiaJavaJedi.java
@@ -0,0 +1,11 @@
+package imperativa;
+
+public class GuiaJavaJedi {
+    public static void main(String[] args) {
+        
+        int numUno = 20;
+        int numDos =30;
+        int resultado = numDos + numUno;
+        System.out.println("El resultado es " + resultado);
+    }
+}
